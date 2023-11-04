0ca4e0cf038c2678c773e270a553a165559c75a0
Pedro Paulo <pedro98seabra@gmail.com>
2023-11-03 21:38:11 -0300

    🎉 Feature: Installation page and component copybutton

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import '../css/app.css'
+
+document.getElementById('myInput').addEventListener('click', copyToClipboard)
+
+function copyToClipboard() {
+  let copyText = document.getElementById('installation')
+  console.log(copyText.textContent)
+
+  navigator.clipboard.writeText(copyText.textContent)
+
+  alert('Copied the text: ' + copyText.textContent)
+}
